6561bbeab1ccf72e28256aeba1a0ae3e3fb8e7a7
Slawek <komodo404@yahoo.com>
2024-11-20 20:26:35 +0100

    POM add to login spec

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -11,7 +11,6 @@ export class LoginPage {
     this.loginInput = this.page.getByTestId('login-input');
     this.passwordInput = this.page.getByTestId('password-input');
     this.loginButton = this.page.getByTestId('login-button');
-
     this.loginError = this.page.getByTestId('error-login-id')
     this.passwordError = this.page.getByTestId('error-login-password')
   }
